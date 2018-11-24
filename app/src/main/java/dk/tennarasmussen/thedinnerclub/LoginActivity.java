d5b241c82ccb61a938a74fca47f22a3b3b964641
15aber <tenna93@hotmail.com>
2018-11-24 20:28:30 +0100

    styles created and firebase auth included

--- app/src/main/java/dk/tennarasmussen/thedinnerclub/LoginActivity.java
+++ app/src/main/java/dk/tennarasmussen/thedinnerclub/LoginActivity.java
@@ -2,12 +2,29 @@ package dk.tennarasmussen.thedinnerclub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    String Log = "LoginActivity";
+
+    //Views
+    EditText etEmail, etPassword;
+    Button btnLogin;
+    TextView tvbtnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etLoginEmail);
+        etPassword = findViewById(R.id.etLoginPass);
+        btnLogin = findViewById(R.id.btnLoginLogin);
+        tvbtnRegister = findViewById(R.id.btnLoginRegister);
+
+
     }
 }
