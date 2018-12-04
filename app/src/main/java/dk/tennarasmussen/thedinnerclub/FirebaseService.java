54519e76f001d6fe0b0a7dcee43c5cabf991f80b
15aber <tenna93@hotmail.com>
2018-12-04 09:28:46 +0100

    Fixing weird errors

--- app/src/main/java/dk/tennarasmussen/thedinnerclub/FirebaseService.java
+++ app/src/main/java/dk/tennarasmussen/thedinnerclub/FirebaseService.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
